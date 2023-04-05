fb2725caf7ca086cb414694dc0bec91c2015c41d
Bugaddr <Bugaddr@protonmail.com>
2023-04-06 03:33:49 +0530

    Create airgeddon-req.sh

---
+++ script/airgeddon-req.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# CHECK ROOT
+if ((EUID != 0)); then
+    echo -e "\033[31;5;7mPlease run as root\033[0m"
+    exit
+fi
+
+# INSTALL REQUIRED STUFF
+paru -S \
+    aircrack-ng \
+    asleap \
+    bettercap \
+    bully \
+    ccze \
+    crunch \
+    dhcp \
+    dnsmasq \
+    ethtool \
+    ettercap \
+    gawk \
+    hashcat \
+    hcxdumptool \
+    hcxtools \
+    hostapd \
+    hostapd-wpe \
+    iproute2 \
+    iptables \
+    iw \
+    john \
+    lighttpd \
+    mdk3 \
+    mdk4 \
+    nftables \
+    openssl \
+    pciutils \
+    pixiewps \
+    procps-ng \
+    reaver \
+    rfkill \
+    tmux \
+    usbutils \
+    wget \
+    xorg-xdpyinfo \
+    xorg-xset \
+    xterm # wireshark-cli beef
