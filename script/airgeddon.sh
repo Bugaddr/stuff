2508495f47591201a59e30eb702844c504fee82c
Bugaddr <Bugaddr@protonmail.com>
2023-04-06 03:33:49 +0530

    Use seperate install mechanism

--- script/airgeddon.sh
+++ script/airgeddon.sh
@@ -1,54 +1,53 @@
 #!/usr/bin/env bash
 
-# CHECK ROOT
-if [ "$EUID" -ne 0 ]; then
-    echo -e "\033[31;5;7mPlease run as root\033[0m" && exit
-    if : >/dev/tcp/8.8.8.8/53; then
-        echo ''
-    else echo 'offline Please connect...' && exit; fi
+# Check Net+Root
+if : >/dev/tcp/8.8.8.8/53; then
+  echo 'Internet is ok...'
+else
+  echo 'Offline Please connect...' && exit
 fi
 
-# some var
-export SUSER="${SUDO_USER:-${USER}}"
-noroot(){
-		sudo -H -u "$SUSER" bash -c "$1"
-}
+# Required stuff list
+apps=(
+  'aircrack-ng'
+  'asleap'
+  'bettercap'
+  'bully'
+  'ccze'
+  'crunch'
+  'dhcp'
+  'dnsmasq'
+  'ethtool'
+  'ettercap'
+  'gawk'
+  'hashcat'
+  'hcxdumptool'
+  'hcxtools'
+  'hostapd'
+  'hostapd-wpe'
+  'iproute2'
+  'iptables'
+  'iw'
+  'john'
+  'lighttpd'
+  'mdk3'
+  'mdk4'
+  'nftables'
+  'openssl'
+  'pciutils'
+  'pixiewps'
+  'procps-ng'
+  'reaver'
+  'rfkill'
+  'tmux'
+  'usbutils'
+  'wget'
+  'xorg-xdpyinfo'
+  'xorg-xset'
+  'xterm' # wireshark-cli beef
+)
 
-# INSTALL REQUIRED STUFF
-paru -Syu \
-    aircrack-ng \
-    asleap \
-    bettercap \
-    bully \
-    ccze \
-    crunch \
-    dhcp \
-    dnsmasq \
-    ethtool \
-    ettercap \
-    gawk \
-    hashcat \
-    hcxdumptool \
-    hcxtools \
-    hostapd \
-    hostapd-wpe \
-    iproute2 \
-    iptables \
-    iw \
-    john \
-    lighttpd \
-    mdk3 \
-    mdk4 \
-    nftables \
-    openssl \
-    pciutils \
-    pixiewps \
-    procps-ng \
-    reaver \
-    rfkill \
-    tmux \
-    usbutils \
-    wget \
-    xorg-xdpyinfo \
-    xorg-xset \
-    xterm # wireshark-cli beef
+# Debloater
+for APPP in "${apps[@]}"; do
+  paru -S $APPP
+done
